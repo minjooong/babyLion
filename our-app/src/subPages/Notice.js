48f6effe61664ee2e9532dccba8f7e39fd90063a
limsohyun <limgm7@gmail.com>
2023-06-14 21:43:03 +0900

    nav error fixed

--- our-app/src/subPages/Notice.js
+++ our-app/src/subPages/Notice.js
@@ -6,15 +6,15 @@ import App from './App';
 import About from './About';
 import Club from './Club';
 import Location from './Location';
-//import My from './My';
+import My from './My';
 import New from './New';
-import Notice from './Notice';
+//import Notice from './Notice';
 import Nav from "../nav";
 
 function Notice() {
     return (
         <div>
-            <Nav/>
+            <Nav />
         </div>
     )
 }
