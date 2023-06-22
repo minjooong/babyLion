4007d725f6fec8fbb9a55b78b676f17f4671b94d
Chamingyeong <chamingyeong10221022@gmail.com>
2023-06-23 02:50:11 +0900

    location

--- our-app/src/subPages/Location.js
+++ our-app/src/subPages/Location.js
@@ -17,12 +17,15 @@ import button1Image from './location_1f.jpg';
 import button2Image from './location_2f.jpg';
 import button3Image from './location_3f.jpg';
 import button4Image from './location_45f.jpg';
+import defaultImage from './default_image.jpg';
 
 function Location() {
   const [selectedButton, setSelectedButton] = useState(null);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   const handleButtonClick = (buttonId) => {
     setSelectedButton(buttonId);
+    setIsButtonClicked(true);
   };
 
   const imagePaths = {
@@ -46,44 +49,57 @@ function Location() {
       >
         학생회관 동방 위치
       </h1>
+
       <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px' }}>
         <button
-          className={`inline-block rounded bg-violet-500 px-8 py-3 text-sm font-medium text-white transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-violet-500 ${
-            selectedButton === 'button1' ? 'selected' : ''
-          }`}
+          className={`inline-block rounded bg-violet-500 px-8 py-3 text-sm font-medium text-white transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-violet-500 ${selectedButton === 'button1' ? 'selected' : ''
+            }`}
           style={{ flex: '1', marginRight: '30px' }}
           onClick={() => handleButtonClick('button1')}
         >
           1F
         </button>
         <button
-          className={`inline-block rounded bg-gray-100 px-8 py-3 text-sm font-medium text-violet-500 transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:text-violet-500 ${
-            selectedButton === 'button2' ? 'selected' : ''
-          }`}
+          className={`inline-block rounded bg-gray-100 px-8 py-3 text-sm font-medium text-violet-500 transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:text-violet-500 ${selectedButton === 'button2' ? 'selected' : ''
+            }`}
           style={{ flex: '1', marginRight: '30px' }}
           onClick={() => handleButtonClick('button2')}
         >
           2F
         </button>
         <button
-          className={`inline-block rounded bg-violet-500 px-8 py-3 text-sm font-medium text-white transition hover:-rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-violet-500 ${
-            selectedButton === 'button3' ? 'selected' : ''
-          }`}
+          className={`inline-block rounded bg-violet-500 px-8 py-3 text-sm font-medium text-white transition hover:-rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-violet-500 ${selectedButton === 'button3' ? 'selected' : ''
+            }`}
           style={{ flex: '1', marginRight: '30px' }}
           onClick={() => handleButtonClick('button3')}
         >
           3F
         </button>
         <button
-          className={`inline-block rounded bg-gray-100 px-8 py-3 text-sm font-medium text-violet-500 transition hover:-rotate-2 hover:scale-110 focus:outline-none focus:ring active:text--500 ${
-            selectedButton === 'button4' ? 'selected' : ''
-          }`}
+          className={`inline-block rounded bg-gray-100 px-8 py-3 text-sm font-medium text-violet-500 transition hover:-rotate-2 hover:scale-110 focus:outline-none focus:ring active:text--500 ${selectedButton === 'button4' ? 'selected' : ''
+            }`}
           style={{ flex: '1', marginRight: '30px' }}
           onClick={() => handleButtonClick('button4')}
         >
-          4.5F
+          4,5F
         </button>
       </div>
+      {!selectedButton && !isButtonClicked && (
+        <div style={{ textAlign: 'center', marginTop: '30px', padding: '30px' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <img
+              src={defaultImage}
+              alt="Default Image"
+              style={{
+                display: 'inline-block',
+                margin: '0',
+                maxWidth: '100%',
+                maxHeight: '100%',
+              }}
+            />
+          </div>
+        </div>
+      )}
       {selectedButton && (
         <div style={{ textAlign: 'center', marginTop: '30px', padding: '30px' }}>
           <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
