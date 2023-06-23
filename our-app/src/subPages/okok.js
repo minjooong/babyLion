180010ad56d2861afa2650df487c6ef714be2356
okryun <memoria0124@gmail.com>
2023-06-23 15:26:57 +0900

    delete module

---
+++ our-app/src/subPages/okok.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import '../index';
+import App from './App';
+import About from './About';
+import Club from './Club';
+import Location from './Location';
+import My from './My';
+//import New from './New';
+import Notice from './Notice';
+import button5Image from './panda1.gif';
+import Nav from '../nav';
+
+function Okok() {
+    return (
+        <div>
+            <Nav /></div>
+
+
+                );
+}
+
+                export default Okok;
