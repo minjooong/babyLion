1372ae8e414e2e3589447497225a26d2d0943667
JUN <xojun412@gmail.com>
2023-06-23 16:06:25 +0900

    NW edit

--- our-app/src/subPages/NewWrite.js
+++ our-app/src/subPages/NewWrite.js
@@ -15,48 +15,64 @@ function NewWrite() {
     return (
         <div>
             <Nav />
-            <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-                <div class="mx-auto max-w-lg text-center">
-                    <h1 class="text-2xl font-bold sm:text-3xl text-violet-500">글쓰기</h1>
+            <div class="bg-gray-100">
+                <div class="mx-auto max-w-full px-4 py-16 sm:px-6 lg:px-8">
+                    <div class="mx-auto w-9/12 text-center">
+                        <h1 class="text-2xl font-bold sm:text-3xl text-violet-500">글쓰기</h1>
 
+                    </div>
+
+                    <form action="" class="mx-auto mb-0 mt-8 w-9/12 space-y-4">
+                        <div>
+                            <label for="title" class="sr-only">Title</label>
+
+                            <div class="relative">
+                                <input
+                                    type="title"
+                                    class="w-full border-2 rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                    placeholder="제목"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label for="content" class="sr-only">Content</label>
+
+                            <div class="relative">
+                                <textarea
+                                    class="w-full border-2 rounded-lg border-gray-200 p-3 text-sm"
+                                    placeholder="내용을 입력하세요."
+                                    rows="20"
+                                    id="message"
+                                ></textarea>
+                            </div>
+                        </div>
+                        <div class="flex justify-end">
+                            <p class="p-1 mr-2">익명</p>
+                            <label for="AcceptConditions" class="relative h-8 w-14 cursor-pointer">
+                                <input type="checkbox" id="AcceptConditions" class="peer sr-only" />
+
+                                <span
+                                    class="absolute inset-0 rounded-full bg-gray-300 transition peer-checked:bg-violet-500"
+                                ></span>
+
+                                <span
+                                    class="absolute inset-y-0 start-0 m-1 h-6 w-6 rounded-full bg-white transition-all peer-checked:start-6"
+                                ></span>
+                            </label>
+                        </div>
+                        <div class="flex justify-end">
+                            <button
+                                type="submit"
+                                class="inline-block rounded-lg bg-violet-500 px-5 py-3 text-sm font-medium text-white"
+                            >
+                                완료
+                            </button>
+                        </div>
+                    </form>
                 </div>
-
-                <form action="" class="mx-auto mb-0 mt-8 max-w-md space-y-4">
-                    <div>
-                        <label for="title" class="sr-only">Title</label>
-
-                        <div class="relative">
-                            <input
-                                type="title"
-                                class="w-full border-2 rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                                placeholder="제목"
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <label for="content" class="sr-only">Content</label>
-
-                        <div class="relative">
-                            <textarea
-                                class="w-full border-2 rounded-lg border-gray-200 p-3 text-sm"
-                                placeholder="내용을 입력하세요."
-                                rows="8"
-                                id="message"
-                            ></textarea>
-                        </div>
-                    </div>
-
-                    <div class="flex justify-end">
-                        <button
-                            type="submit"
-                            class="inline-block rounded-lg bg-violet-500 px-5 py-3 text-sm font-medium text-white"
-                        >
-                            완료
-                        </button>
-                    </div>
-                </form>
             </div>
+
         </div>
     );
 }
